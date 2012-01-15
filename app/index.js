16c187c830db1f060ca8338bdcaed713124601de
Adrian <adriankue@gmail.com>
2012-01-15 19:25:57 +0100

    First commit comming from backbonejs boilerplate plus some additional submodules containing plugins for requirejs

---
+++ app/index.js
@@ -0,0 +1,115 @@
+define("namespace", [
+  "jquery",
+  "use!underscore",
+  "use!backbone"
+],
+
+function($) {
+  return {
+    // This is useful when developing if you don't want to use a
+    // build process every time you change a template.
+    //
+    // Delete if you are using a different template loading method.
+    fetchTemplate: function(path, done) {
+      // Should be an instant synchronous way of getting the template, if it
+      // exists in the JST object.
+      var JST = this.JST = this.JST || {};
+      if (JST[path]) {
+        return done(JST[path]);
+      }
+
+      // Fetch it asynchronously if not available from JST
+      return $.get(path, function(contents) {
+        var tmpl = _.template(contents);
+        JST[path] = tmpl;
+        done(tmpl);
+      });
+    },
+
+    // Create a custom object with a nested Views object
+    module: function(additionalProps) {
+      return _.extend({ Views: {} }, additionalProps);
+    },
+
+    // Keep active application instances namespaced under an app object.
+    app: _.extend({}, Backbone.Events)
+  };
+});
+
+require([
+  "namespace",
+  "jquery",
+  "use!backbone",
+  "modules/example"
+],
+
+function (namespace, jQuery, Backbone, Example) {
+  // Treat the jQuery ready function as the entry point to the application.
+  // Inside this function, kick-off all initialization, everything up to this
+  // point should be definitions.
+  jQuery(function($) {
+
+    // Shorthand the application namespace
+    var app = namespace.app;
+
+    // Defining the application router, you can attach sub routers here.
+    var Router = Backbone.Router.extend({
+      routes: {
+        "": "index",
+        ":hash": "index"
+      },
+
+      index: function(hash) {
+        var route = this;
+        var tutorial = new Example.Views.Tutorial();
+
+        // Attach the tutorial to the DOM
+        tutorial.render(function(el) {
+          $("#main").html(el);
+
+          // Fix for hashes in pushState and hash fragment
+          if (hash && !route._alreadyTriggered) {
+            // Reset to home, pushState support automatically converts hashes
+            Backbone.history.navigate("", false);
+
+            // Trigger the default browser behavior
+            location.hash = hash;
+
+            // Set an internal flag to stop recursive looping
+            route._alreadyTriggered = true;
+          }
+        });
+      }
+    });
+    
+    // Define your master router on the application namespace and trigger all
+    // navigation from this instance.
+    app.router = new Router();
+
+    // Trigger the initial route and enable HTML5 History API support
+    Backbone.history.start({ pushState: true });
+
+    // All navigation that is relative should be passed through the navigate
+    // method, to be processed by the router.  If the link has a data-bypass
+    // attribute, bypass the delegation completely.
+    $(document).on("click", "a:not([data-bypass])", function(evt) {
+      // Get the anchor href and protcol
+      var href = $(this).attr("href");
+      var protocol = this.protocol + "//";
+
+      // Ensure the protocol is not part of URL, meaning its relative.
+      if (href.slice(protocol.length) !== protocol) {
+        // Stop the default event to ensure the link will not cause a page
+        // refresh.
+        evt.preventDefault();
+
+        // This uses the default router defined above, and not any routers
+        // that may be placed in modules.  To have this work globally (at the
+        // cost of losing all route events) you can change the following line
+        // to: Backbone.history.navigate(href, true);
+        app.router.navigate(href, true);
+      }
+    });
+
+  });
+});
---
+++ build/config.js
@@ -0,0 +1,89 @@
+// This is the main Backbone Boilerplate build configuration file.
+
+// Custom function to read in require.config settings
+function readRequireConfig(path) {
+  var _require = require;
+  var obj;
+  var config = require("fs").readFileSync(path).toString();
+
+  // Patch over require since jshint complains about using with...
+  require = {
+    config: function(_obj) {
+      obj = _obj;
+    }
+  };
+
+  // Yes I know what this is doing...
+  eval(config);
+
+  // Restore require
+  require = _require;
+
+  return obj || {};
+}
+
+// This is a JavaScript file, you can define any functions you would like in
+// here.
+config.init({
+
+  clean: {
+    folder: "dist/"
+  },
+
+  jst: {
+    "dist/debug/js/templates.js": ["app/templates/*.html"]
+  },
+
+  lint: {
+    files: ["build/config.js", "app/**/*.js"]
+  },
+
+  jshint: {
+    options: {
+      evil: true
+    }
+  },
+
+  watch: {
+    files: ["app/**/*", "assets/**/*"],
+    tasks: "lint:files requirejs",
+    
+    min: {
+      files: "<watch:files>",
+      tasks: "default min mincss"
+    }
+  },
+
+  mincss: {
+    "dist/release/style.css": ["assets/css/**/*.css"]
+  },
+
+  min: {
+    "dist/release/index.js": ["dist/debug/index.js"],
+    "dist/release/require.js": ["assets/js/libs/require.js"]
+  },
+
+  server: {
+    debug: {
+      paths: {
+        "app": "dist/debug",
+        "app/templates": "app/templates"
+      }
+    },
+
+    release: {
+      paths: {
+        "app": "dist/release",
+        "app/templates": "app/templates",
+        "assets/js/libs": "dist/release",
+        "assets/css": "dist/release"
+      }
+    }
+  },
+
+  requirejs: readRequireConfig("app/config.js")
+
+});
+
+// Run the following tasks...
+task.registerTask("default", "clean lint requirejs jst");
